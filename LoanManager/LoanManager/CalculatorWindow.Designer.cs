a323db6cf2794fd028c378d89b1a5c263ccdcf94
Brian Ries <briantries@gmail.com>
2011-08-07 21:04:21 -0500

    updates to the calculator window

--- LoanManager/LoanManager/CalculatorWindow.Designer.cs
+++ LoanManager/LoanManager/CalculatorWindow.Designer.cs
@@ -34,19 +34,18 @@
             this.label2 = new System.Windows.Forms.Label();
             this.inputNumPayments = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.calculatePrincipleButton = new System.Windows.Forms.Button();
             this.inputMonthlyPayment = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.calculateAPRButton = new System.Windows.Forms.Button();
-            this.calculateNumPaymentsButton = new System.Windows.Forms.Button();
-            this.calculateMonthlyPaymentButton = new System.Windows.Forms.Button();
+            this.generateSchedule = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Location = new System.Drawing.Point(16, 29);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(62, 17);
             this.label1.TabIndex = 0;
@@ -54,25 +53,25 @@
             // 
             // inputPrinciple
             // 
-            this.inputPrinciple.Location = new System.Drawing.Point(135, 8);
+            this.inputPrinciple.Location = new System.Drawing.Point(139, 28);
             this.inputPrinciple.Name = "inputPrinciple";
             this.inputPrinciple.Size = new System.Drawing.Size(92, 20);
             this.inputPrinciple.TabIndex = 1;
-            this.inputPrinciple.ModifiedChanged += new System.EventHandler(this.inputPrinciple_Enter);
+            this.inputPrinciple.TextChanged += new System.EventHandler(this.inputPrinciple_Changed);
             // 
             // inputAPR
             // 
-            this.inputAPR.Location = new System.Drawing.Point(135, 54);
+            this.inputAPR.Location = new System.Drawing.Point(139, 74);
             this.inputAPR.Name = "inputAPR";
             this.inputAPR.Size = new System.Drawing.Size(92, 20);
             this.inputAPR.TabIndex = 3;
-            this.inputAPR.ModifiedChanged += new System.EventHandler(this.inputAPR_ModifiedChanged);
+            this.inputAPR.TextChanged += new System.EventHandler(this.inputAPR_Changed);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(12, 55);
+            this.label2.Location = new System.Drawing.Point(16, 75);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(36, 17);
             this.label2.TabIndex = 2;
@@ -80,99 +79,76 @@
             // 
             // inputNumPayments
             // 
-            this.inputNumPayments.Location = new System.Drawing.Point(135, 101);
+            this.inputNumPayments.Location = new System.Drawing.Point(139, 121);
             this.inputNumPayments.Name = "inputNumPayments";
             this.inputNumPayments.Size = new System.Drawing.Size(92, 20);
             this.inputNumPayments.TabIndex = 5;
-            this.inputNumPayments.ModifiedChanged += new System.EventHandler(this.inputNumPayments_ModifiedChanged);
+            this.inputNumPayments.TextChanged += new System.EventHandler(this.inputNumPayments_Changed);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(12, 102);
+            this.label3.Location = new System.Drawing.Point(16, 122);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(82, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "# Payments";
             // 
-            // calculatePrincipleButton
-            // 
-            this.calculatePrincipleButton.Location = new System.Drawing.Point(233, 4);
-            this.calculatePrincipleButton.Name = "calculatePrincipleButton";
-            this.calculatePrincipleButton.Size = new System.Drawing.Size(68, 27);
-            this.calculatePrincipleButton.TabIndex = 6;
-            this.calculatePrincipleButton.Text = "Calculate";
-            this.calculatePrincipleButton.UseVisualStyleBackColor = true;
-            this.calculatePrincipleButton.Click += new System.EventHandler(this.calculatePrincipleButton_Click);
-            // 
             // inputMonthlyPayment
             // 
-            this.inputMonthlyPayment.Location = new System.Drawing.Point(135, 146);
+            this.inputMonthlyPayment.Location = new System.Drawing.Point(149, 188);
             this.inputMonthlyPayment.Name = "inputMonthlyPayment";
             this.inputMonthlyPayment.Size = new System.Drawing.Size(92, 20);
             this.inputMonthlyPayment.TabIndex = 8;
-            this.inputMonthlyPayment.ModifiedChanged += new System.EventHandler(this.inputMonthlyPayment_ModifiedChanged);
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(12, 147);
+            this.label4.Location = new System.Drawing.Point(15, 189);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(116, 17);
             this.label4.TabIndex = 7;
             this.label4.Text = "Monthly Payment";
             // 
-            // calculateAPRButton
+            // generateSchedule
             // 
-            this.calculateAPRButton.Location = new System.Drawing.Point(233, 50);
-            this.calculateAPRButton.Name = "calculateAPRButton";
-            this.calculateAPRButton.Size = new System.Drawing.Size(68, 27);
-            this.calculateAPRButton.TabIndex = 9;
-            this.calculateAPRButton.Text = "Calculate";
-            this.calculateAPRButton.UseVisualStyleBackColor = true;
-            this.calculateAPRButton.Click += new System.EventHandler(this.calculateAPRButton_Click);
+            this.generateSchedule.Location = new System.Drawing.Point(18, 229);
+            this.generateSchedule.Name = "generateSchedule";
+            this.generateSchedule.Size = new System.Drawing.Size(247, 27);
+            this.generateSchedule.TabIndex = 11;
+            this.generateSchedule.Text = "Generate Schedule";
+            this.generateSchedule.UseVisualStyleBackColor = true;
             // 
-            // calculateNumPaymentsButton
+            // groupBox1
             // 
-            this.calculateNumPaymentsButton.Location = new System.Drawing.Point(233, 97);
-            this.calculateNumPaymentsButton.Name = "calculateNumPaymentsButton";
-            this.calculateNumPaymentsButton.Size = new System.Drawing.Size(68, 27);
-            this.calculateNumPaymentsButton.TabIndex = 10;
-            this.calculateNumPaymentsButton.Text = "Calculate";
-            this.calculateNumPaymentsButton.UseVisualStyleBackColor = true;
-            this.calculateNumPaymentsButton.Click += new System.EventHandler(this.calculateNumPaymentsButton_Click);
-            // 
-            // calculateMonthlyPaymentButton
-            // 
-            this.calculateMonthlyPaymentButton.Location = new System.Drawing.Point(233, 142);
-            this.calculateMonthlyPaymentButton.Name = "calculateMonthlyPaymentButton";
-            this.calculateMonthlyPaymentButton.Size = new System.Drawing.Size(68, 27);
-            this.calculateMonthlyPaymentButton.TabIndex = 11;
-            this.calculateMonthlyPaymentButton.Text = "Calculate";
-            this.calculateMonthlyPaymentButton.UseVisualStyleBackColor = true;
-            this.calculateMonthlyPaymentButton.Click += new System.EventHandler(this.calculateMonthlyPaymentButton_Click);
+            this.groupBox1.Controls.Add(this.inputNumPayments);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.inputAPR);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.inputPrinciple);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(10, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(255, 168);
+            this.groupBox1.TabIndex = 14;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Loan Parameters";
             // 
             // CalculatorWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(309, 183);
-            this.Controls.Add(this.calculateMonthlyPaymentButton);
-            this.Controls.Add(this.calculateNumPaymentsButton);
-            this.Controls.Add(this.calculateAPRButton);
+            this.ClientSize = new System.Drawing.Size(278, 270);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.generateSchedule);
             this.Controls.Add(this.inputMonthlyPayment);
             this.Controls.Add(this.label4);
-            this.Controls.Add(this.calculatePrincipleButton);
-            this.Controls.Add(this.inputNumPayments);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.inputAPR);
-            this.Controls.Add(this.label2);
-            this.Controls.Add(this.inputPrinciple);
-            this.Controls.Add(this.label1);
             this.Name = "CalculatorWindow";
             this.Text = "Calculator";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -186,11 +162,9 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox inputNumPayments;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Button calculatePrincipleButton;
         private System.Windows.Forms.TextBox inputMonthlyPayment;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button calculateAPRButton;
-        private System.Windows.Forms.Button calculateNumPaymentsButton;
-        private System.Windows.Forms.Button calculateMonthlyPaymentButton;
+        private System.Windows.Forms.Button generateSchedule;
+        private System.Windows.Forms.GroupBox groupBox1;
     }
 }
--- LoanManager/LoanManager/CalculatorWindow.cs
+++ LoanManager/LoanManager/CalculatorWindow.cs
@@ -6,6 +6,7 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using System.Text.RegularExpressions;
 
 namespace LoanManager
 {
@@ -16,96 +17,196 @@ namespace LoanManager
         private double _APR = 0;
         private double _monthlyPayment = 0;
 
+
+        public struct CurrencyValue
+        {
+            public string valueString;
+            public double value;
+        }
+
         public CalculatorWindow()
         {
             InitializeComponent();
 
-            this.calculateAPRButton.Enabled = false;
-            this.calculateMonthlyPaymentButton.Enabled = false;
-            this.calculateNumPaymentsButton.Enabled = false;
-            this.calculatePrincipleButton.Enabled = false;    
+            this.generateSchedule.Enabled = false;
         }
 
 
-        private void updateButtonStatuses()
+        private void UpdateMonthlyPayment()
         {
-            this.calculateAPRButton.Enabled = false;
-            this.calculateMonthlyPaymentButton.Enabled = false;
-            this.calculateNumPaymentsButton.Enabled = false;
-            this.calculatePrincipleButton.Enabled = false;
+            this.generateSchedule.Enabled = false;
 
-            Console.WriteLine("Updating button statuses!");
-
-            if (_principle != 0 && _numPayments != 0 && _APR != 0)
+            if (_principle != 0 && _APR != 0 && _numPayments != 0)
             {
-                this.calculateMonthlyPaymentButton.Enabled = true;
-            }
-        }
+                _monthlyPayment = AmortizationSchedule.getInstance().calculateMonthlyPayment((int)_numPayments, _APR, _principle);
+                this.generateSchedule.Enabled = true;
 
-        private void inputPrinciple_Enter(object sender, EventArgs e)
-        {
-            double result;
-            if (double.TryParse(this.inputAPR.Text, out result))
-            {
-                _principle = result;
             }
 
-            updateButtonStatuses();
+            UpdateMonthlyPayment(_monthlyPayment.ToString());
         }
 
-        private void inputAPR_ModifiedChanged(object sender, EventArgs e)
+        private string AddCommas(string input)
         {
-            double result;
-            if (double.TryParse(this.inputAPR.Text, out result))
+            string newString = "";
+            int counter = 0;
+            int location = input.Length-1;
+          
+            while(location >= 0)
             {
-                _APR = result;
+                if (counter%3 == 0 && counter != 0)
+                {
+                    newString = "," + newString;
+                }
+
+                char val = input[location];
+                counter++;
+                location--;
+
+                newString = val + newString;
+            }
+           
+            return newString;
+        }
+
+        private CurrencyValue ReturnFormatedCurrency(int leftDecimalPlaces, int rightDecimalPlaces, string inputStr)
+        {
+            //Find the decimal point
+            int index = inputStr.IndexOf(".");
+
+            string wholeNumber = inputStr;
+            string fraction = "";
+
+            //If there is a decimal point
+            if (index > 0)
+            {
+                //The whole number is to the left
+                wholeNumber = inputStr.Substring(0, index);
+
+                //If there is any more string to the right, that's the fraction
+                if (index+1 < inputStr.Length)
+                {
+                    fraction = inputStr.Substring(index+1);
+
+                  
+                }
+            }
+           
+            //Finds all non-digit characters and replaces with ""
+            string strippedWhole = Regex.Replace(wholeNumber, @"[^0-9]", "");
+            string strippedFraction = Regex.Replace(fraction, @"[^0-9]", "");
+
+            //Trim the whole number to the provided decimal places
+            if (strippedWhole.Length > leftDecimalPlaces)
+            {
+                strippedWhole = strippedWhole.Substring(0, leftDecimalPlaces);
             }
 
-            updateButtonStatuses();
-        }
-
-        private void inputNumPayments_ModifiedChanged(object sender, EventArgs e)
-        {
-            double result;
-            if (double.TryParse(this.inputAPR.Text, out result))
+            //Trim the fraction to 2 decimal places
+            if (strippedFraction.Length > rightDecimalPlaces)
             {
-                _numPayments = result;
+                strippedFraction = strippedFraction.Substring(0, rightDecimalPlaces);
             }
 
-            updateButtonStatuses();
-        }
-
-        private void inputMonthlyPayment_ModifiedChanged(object sender, EventArgs e)
-        {
-            double result;
-            if (double.TryParse(this.inputAPR.Text, out result))
+            //Combine the values back
+            string combinedNumeric = null;
+            string combinedCommas = null;
+            if ((strippedFraction.Equals("") == false || index > 0) && rightDecimalPlaces > 0)
             {
-                _monthlyPayment = result;
+                combinedNumeric = strippedWhole + "." + strippedFraction;
+                combinedCommas = AddCommas(strippedWhole) + "." + strippedFraction;
+            }
+            else
+            {
+                combinedNumeric = strippedWhole;
+                combinedCommas = AddCommas(strippedWhole);
             }
 
-            updateButtonStatuses();
+            //Parse the value while we have it
+            double value;
+            if (!double.TryParse(combinedNumeric, out value))
+            {
+                Console.WriteLine("ERROR: Couldn't parse string {0} to value!", combinedNumeric);
+            }
+
+            CurrencyValue currValue;
+            currValue.value = value;
+            currValue.valueString = combinedCommas;
+
+            Console.WriteLine("String = {0} Value = {1}", currValue.valueString, currValue.value);
+
+            return currValue;
         }
 
-        private void calculatePrincipleButton_Click(object sender, EventArgs e)
+
+        private void inputPrinciple_Changed(object sender, EventArgs e)
+        {
+            UpdatePrinciple(this.inputPrinciple.Text);
+
+            UpdateMonthlyPayment();
+        }
+
+        private void inputAPR_Changed(object sender, EventArgs e)
+        {
+            UpdateAPR(this.inputAPR.Text);
+
+            UpdateMonthlyPayment();
+        }
+
+        private void inputNumPayments_Changed(object sender, EventArgs e)
+        {
+            UpdateNumPayments(this.inputNumPayments.Text);
+          
+            UpdateMonthlyPayment();
+        }
+
+        private void UpdatePrinciple(string text)
+        {
+            CurrencyValue curr = ReturnFormatedCurrency(8, 2, text);
+
+            this._principle = curr.value;
+
+            //Set the text box to display this new value and set the cursor to the end
+            this.inputPrinciple.Text = curr.valueString;
+            this.inputPrinciple.Select(curr.valueString.Length, 0);
+        }
+
+        private void UpdateAPR(string text)
+        {
+            CurrencyValue curr = ReturnFormatedCurrency(2, 2, text);
+
+            this._APR = curr.value;
+
+            //Set the text box to display this new value and set the cursor to the end
+            this.inputAPR.Text = curr.valueString;
+            this.inputAPR.Select(curr.valueString.Length, 0);
+        }
+
+        private void UpdateNumPayments(string text)
+        {
+            CurrencyValue curr = ReturnFormatedCurrency(4, 0, text);
+
+            this._numPayments = curr.value;
+
+            //Set the text box to display this new value and set the cursor to the end
+            this.inputNumPayments.Text = curr.valueString;
+            this.inputNumPayments.Select(curr.valueString.Length, 0);
+        }
+
+        private void UpdateMonthlyPayment(string text)
+        {
+            CurrencyValue curr = ReturnFormatedCurrency(7, 2, text);
+
+            this._monthlyPayment = curr.value;
+
+            //Set the text box to display this new value and set the cursor to the end
+            this.inputMonthlyPayment.Text = curr.valueString;
+            this.inputMonthlyPayment.Select(curr.valueString.Length, 0);
+        }
+
+        private void generateScheduleButton_Click(object sender, EventArgs e)
         {
 
         }
-
-        private void calculateAPRButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void calculateNumPaymentsButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void calculateMonthlyPaymentButton_Click(object sender, EventArgs e)
-        {
-
-        }
-
-       
     }
 }
